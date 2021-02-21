a8e1c8a79859e8ff6a8ddd9e0e11333b58afbb5e
Prasanna <prasannacse2005@gmail.com>
2021-02-21 07:06:17 +0530

    :tada: Bare minimum sbt project

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "akka-cluster-k8s"
+
+version := "0.1"
+
+scalaVersion := "2.11.11"
---
+++ src/main/scala/com/tryout/akkas/Boot.scala
@@ -0,0 +1,5 @@
+package com.tryout.akkas
+
+object Boot extends App {
+  println("Hello Akka & Kubernetes")
+}
